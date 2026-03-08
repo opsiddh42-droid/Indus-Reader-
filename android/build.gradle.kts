854bb6b477a73ccd96b7d5fa7513a013ea937925
opsiddh42-droid <opsiddh42@gmail.com>
2026-03-08 13:56:30 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,12 +15,21 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 
-    // --- THE ULTIMATE FIX: Kotlin ko Java ke default (11) ke sath match kar diya ---
+    // --- FINAL MASTERSTROKE: afterEvaluate ko evaluationDependsOn se pehle lagaya hai ---
+    // Isse dono Java aur Kotlin 100% force hoke 17 par aa jayenge bina kisi clash ke!
+    afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+    }
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
+// Yeh hamesha afterEvaluate ke BAAD aana chahiye, taaki error na aaye
 subprojects {
     project.evaluationDependsOn(":app")
 }

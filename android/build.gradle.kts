c0731cb10599f911998a22d3fa4bbd5fc1846385
opsiddh42-droid <opsiddh42@gmail.com>
2026-03-08 13:06:31 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,30 +14,6 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-
-    // --- SURGICAL STRIKE: Jis plugin ko jo Java version chahiye, wahi do ---
-    tasks.withType<JavaCompile>().configureEach {
-        if (project.name == "receive_sharing_intent") {
-            sourceCompatibility = "1.8"
-            targetCompatibility = "1.8"
-        } else if (project.name == "file_picker") {
-            sourceCompatibility = "11"
-            targetCompatibility = "11"
-        } else {
-            sourceCompatibility = "17"
-            targetCompatibility = "17"
-        }
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        if (project.name == "receive_sharing_intent") {
-            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
-        } else if (project.name == "file_picker") {
-            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        } else {
-            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
-    }
 }
 
 subprojects {
@@ -46,4 +22,17 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// --- MASTER FIX: afterEvaluate sabse last me run hoga aur kisi ko manmani nahi karne dega ---
+subprojects {
+    afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions.jvmTarget = "17"
+        }
+    }
 }
